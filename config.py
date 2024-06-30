55fa60aa0c4d08c900a30ee3dfce8b8bcfab8d16
devdsinghh <devworld2006@gmail.com>
2024-06-30 19:22:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/devdsinghh/devmusic/tree/main",
+    "https://github.com/nobodydi/MUSICBABY",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
